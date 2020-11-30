e3a12f444a8e3078bd1dbaabcf1662af228c30fe
LeonardoTruccollo <leonardo@sparkag.com.br>
2020-11-30 15:02:48 -0300

    Adicionado arquivo JS

---
+++ JS/dropDown.js
@@ -0,0 +1,36 @@
+// Função DropDown chamando demoAcc
+function myAccFunc() {
+    var x = document.getElementById("demoAcc");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+// Função DropDown chamando demoAcc2
+function myAccFunc2() {
+    var x = document.getElementById("demoAcc2");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+// Clicando no "Contratar ou Ofertar" vai abrir os submenus
+document.getElementById("myBtn").click();
+document.getElementById("myBtn2").click();
+
+
+
+// Abre e fecha os sideBar
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
